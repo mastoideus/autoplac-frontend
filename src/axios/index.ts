5ae17a287faa8567df97470d78e8c1670b397de5
Anel Zubcevic <anelzubcevic@gmail.com>
2025-06-22 10:26:39 +0200

    url fix for deply

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const customFetch: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://autoplac-backend.onrender.com",
 });
 
 export default customFetch;
